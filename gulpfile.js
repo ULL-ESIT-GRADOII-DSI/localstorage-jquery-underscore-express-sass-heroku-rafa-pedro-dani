6a8a7ec94f7608720b154c4751788be76c6b3ed6
Daniel Ramos <danielramosacosta@hotmail.com>
2016-04-06 21:51:21 +0100

    Añadidas las dependencias al json y quitado un paquete que sobraba

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp'),
+    nodemon = require('gulp-nodemon'),
+    browserSync = require('browser-sync');
+
+// Tarea que debería encapsular todo
+gulp.task('serve', ['browser-sync'], function() {
+    //Compilar SASS y meter en CSS
+});
+
+// Browser sync, espera a que nodemon se reinicie y si lo hace, recarga el
+// navegador del cliente.
+gulp.task('browser-sync', ['nodemon'], function() {
+    browserSync.init(null, {
+        proxy: "http://0.0.0.0:8080",
+        files: ["public/**/*.*", "views/*.ejs"],
+        port: 7000,
+    });
+});
+
+// Esta tarea abre el servidor en el puerto 8080 y lo reinicia si detecta
+// cambios en los ficheros .js, .html y .ejs. TODO: añadir los css
+gulp.task('nodemon', function () {
+    nodemon({
+        script: 'bin/www',
+        ext: 'js html ejs',
+        env: {'NODE_ENV': 'development'}
+    }).on('start', function(){
+        if (!called) {
+            called = true;
+            cb();
+        }
+    });
+});
