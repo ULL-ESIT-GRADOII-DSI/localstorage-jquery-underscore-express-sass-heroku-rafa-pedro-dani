f6816a8366406809e94b5fd7f6190e0bed0d4b80
alu0100505078 <alu0100505078@ull.edu.es>
2016-04-10 14:55:38 +0100

    Prueba en Local funcionando en heroku falla

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 var server = http.createServer(function(req,res){
   "use strict";
   res.writeHead(200,{'Content-Type':'text/html'});
-  res.end('<h1>Hello world</h1>');
+  res.end('<h1>Hola esto es una prueba</h1>');
 });
-var port=Number(process.env.PORT || 3000);
-server.listen(port);
+//var port=Number(process.env.PORT || 3000);
+server.listen(3000);
